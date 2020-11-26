58ae66adf45538dfb9806a23ff02d3d65568a2e5
mryp <picobox.org@gmail.com>
2020-11-26 23:40:16 +0900

    見た目作成

--- FolderToZip/FolderToZip/MainForm.Designer.cs
+++ FolderToZip/FolderToZip/MainForm.Designer.cs
@@ -29,13 +29,126 @@ namespace FolderToZip
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderPathRefButton = new System.Windows.Forms.Button();
+            this.folderPathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.optionUseLevelZeroCheckBox = new System.Windows.Forms.CheckBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressStatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderPathRefButton
+            // 
+            this.folderPathRefButton.Location = new System.Drawing.Point(567, 11);
+            this.folderPathRefButton.Name = "folderPathRefButton";
+            this.folderPathRefButton.Size = new System.Drawing.Size(94, 29);
+            this.folderPathRefButton.TabIndex = 0;
+            this.folderPathRefButton.Text = "参照";
+            this.folderPathRefButton.UseVisualStyleBackColor = true;
+            this.folderPathRefButton.Click += new System.EventHandler(this.folderPathRefButton_Click);
+            // 
+            // folderPathTextBox
+            // 
+            this.folderPathTextBox.Location = new System.Drawing.Point(108, 12);
+            this.folderPathTextBox.Name = "folderPathTextBox";
+            this.folderPathTextBox.Size = new System.Drawing.Size(453, 27);
+            this.folderPathTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "フォルダパス：";
+            // 
+            // optionUseLevelZeroCheckBox
+            // 
+            this.optionUseLevelZeroCheckBox.AutoSize = true;
+            this.optionUseLevelZeroCheckBox.Checked = true;
+            this.optionUseLevelZeroCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.optionUseLevelZeroCheckBox.Location = new System.Drawing.Point(17, 55);
+            this.optionUseLevelZeroCheckBox.Name = "optionUseLevelZeroCheckBox";
+            this.optionUseLevelZeroCheckBox.Size = new System.Drawing.Size(182, 24);
+            this.optionUseLevelZeroCheckBox.TabIndex = 3;
+            this.optionUseLevelZeroCheckBox.Text = "無圧縮ファイルを作成する";
+            this.optionUseLevelZeroCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(17, 93);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(644, 44);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "処理開始";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorker_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker_RunWorkerCompleted);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(17, 143);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(644, 29);
+            this.progressBar.TabIndex = 5;
+            // 
+            // progressStatusLabel
+            // 
+            this.progressStatusLabel.AutoSize = true;
+            this.progressStatusLabel.Location = new System.Drawing.Point(17, 175);
+            this.progressStatusLabel.Name = "progressStatusLabel";
+            this.progressStatusLabel.Size = new System.Drawing.Size(54, 20);
+            this.progressStatusLabel.TabIndex = 6;
+            this.progressStatusLabel.Text = "未処理";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(679, 212);
+            this.Controls.Add(this.progressStatusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.optionUseLevelZeroCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folderPathTextBox);
+            this.Controls.Add(this.folderPathRefButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Folder to Zip";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button folderPathRefButton;
+        private System.Windows.Forms.TextBox folderPathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox optionUseLevelZeroCheckBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressStatusLabel;
     }
 }
 
--- FolderToZip/FolderToZip/MainForm.cs
+++ FolderToZip/FolderToZip/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,75 @@ namespace FolderToZip
 {
     public partial class MainForm : Form
     {
+        private class WorkerParam
+        {
+            public bool IsLevelZero { get; set; } = false;
+            public string FolderPath { get; set; } = "";
+        }
+
         public MainForm()
         {
             InitializeComponent();
         }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderPathRefButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                folderPathTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (bgWorker.IsBusy)
+            {
+                return;
+            }
+
+            var folderPath = folderPathTextBox.Text;
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                MessageBox.Show("フォルダパスが指定されていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!Directory.Exists(folderPath))
+            {
+                MessageBox.Show("フォルダパスが存在しません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            startButton.Enabled = false;
+            bgWorker.RunWorkerAsync(new WorkerParam()
+            {
+                IsLevelZero = optionUseLevelZeroCheckBox.Checked,
+                FolderPath = folderPath,
+            });
+        }
+
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (e.Argument is not WorkerParam param)
+            {
+                return;
+            }
+
+            //param.FolderPath;
+        }
+
+        private void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            startButton.Enabled = true;
+        }
     }
 }
